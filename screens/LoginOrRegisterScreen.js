c7a4e3f046758f2d56610af69240b4b415170d6b
= <=>
2018-10-12 15:35:10 +0200

    geen idee

--- screens/LoginOrRegisterScreen.js
+++ screens/LoginOrRegisterScreen.js
@@ -12,26 +12,24 @@ export default class LoginOrRegisterScreen extends React.Component {
   static navigationOptions = {
     header: null,
   };
-
   render() {
     return (
-      <Container style={styles.container}>
-        <Content padder>
-          <View style={{ alignItems: 'center', marginTop: 75 }}>
-            <Image style={{ width: 256, height: 136 }} source={require('../assets/images/logo-wit.png')} />
-          </View>
-          <View style={{
-            marginTop: 200
-          }}>
-            <Button style={{ marginBottom: 20 }} rounded light block>
-              <Text style={{ fontSize: 18 }}>ik ben al lid</Text>
-            </Button>
-            <Button style={{ marginBottom: 20 }} rounded light block>
-              <Text style={{ fontSize: 18 }}>doe mij die baan</Text>
-            </Button>
-          </View>
-        </Content>
-      </Container>
+      <View style={styles.container}>
+
+        <View style={{ alignItems: 'center', marginTop: 75 }}>
+          <Image style={{ width: 256, height: 136 }} source={require('../assets/images/logo-wit.png')} />
+        </View>
+        <View style={{
+          marginTop: 200
+        }}>
+          <Button style={{ marginBottom: 20 }} rounded light block>
+            <Text style={{ fontSize: 18 }}>ik ben al lid</Text>
+          </Button>
+          <Button style={{ marginBottom: 20 }} rounded light block>
+            <Text style={{ fontSize: 18 }}>doe mij die baan</Text>
+          </Button>
+        </View>
+      </View>
     );
   }
 }
